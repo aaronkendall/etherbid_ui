c23894e294ea98cc861a9e433cd5d5ea521202f8
Aaron <aarongkendall@gmail.com>
2018-06-03 12:25:20 +0100

    extract css loaders added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
-const path = require('path');
-const webpack = require('webpack');
-const environment = process.env.NODE_ENV || 'development';
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const environment = process.env.NODE_ENV || 'development'
 
 const plugins = {
   development: [],
@@ -13,49 +16,67 @@ const plugins = {
   ]
 };
 
+const isDevelopment = environment === 'development'
+
 module.exports = {
-    entry: {
-        index: "./src/app/index.js",
-        style: "./src/app/styles/main.scss"
-    },
-    output: {
-        path: path.resolve(__dirname, './src/server/public/js'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /.js?$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /.scss?$/,
-                use: [
-                  { loader: 'style-loader'},
-                  { loader: 'css-loader'},
-                  { loader: 'sass-loader'}
-                ]
-            },
-            {
-                test: /.json?$/,
-                use: 'json-loader'
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.scss'],
-        modules: [
-            'node_modules',
-            path.resolve(__dirname, './node_modules')
-        ]
-    },
-    plugins: plugins[environment],
-    watch: environment === 'development',
-    mode: environment
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: isDevelopment // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  entry: {
+      index: "./src/app/index.js",
+      style: "./src/app/styles/main.scss"
+  },
+  output: {
+      path: path.resolve(__dirname, './src/server/public/build'),
+      filename: '[name].js'
+  },
+  module: {
+      rules: [
+        {
+            test: /.js?$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        },
+        {
+            test: /.jsx?$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        },
+        {
+            test: /.json?$/,
+            use: 'json-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+  },
+  resolve: {
+      extensions: ['.js', '.jsx', '.scss'],
+      modules: [
+          'node_modules',
+          path.resolve(__dirname, './node_modules')
+      ]
+  },
+  plugins: [
+    ...plugins[environment],
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  watch: isDevelopment,
+  mode: environment
 };
